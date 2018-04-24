3681ea6eb3e6cd3a747c22d76e31877accd9e5f8
Adrien Boutigny <adrien.boutigny@gmail.com>
2018-04-24 15:01:48 +0200

    user list and display

---
+++ src/Controller/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use App\Entity\User;
+
+class UserController extends Controller
+{
+    /**
+     * @Route("/user", name="user_list")
+     */
+    public function listUsers()
+    {
+        $repo = $this->getDoctrine()->getRepository(User::class);
+        $users = $repo->findAll();
+        return $this->render('user/list.html.twig', [
+            'users' => $users
+        ]);
+    }
+
+    /**
+     * @Route("/user/{id}", name="user_swhow", requirements={"id"="\d+"})
+     */
+    public function showUser(User $user)
+    {
+        return $this->render('user/show.html.twig', [
+            'user' => $user
+        ]);
+    }
+}
