77419f5e78b4304fec5193dae89c850781748ed8
Adrien Boutigny <adrien.boutigny@gmail.com>
2018-05-05 23:56:51 +0200

    add Message navigation link

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -11,7 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class MessageController extends Controller
 {
     /**
-     * @Route("/", name="message")
+     * @Route("/", name="message_app")
      */
     public function index()
     {
