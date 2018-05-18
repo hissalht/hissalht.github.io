76eb196788d49922f7fc4225a5b9d15391b23b52
Adrien Boutigny <adrien.boutigny@gmail.com>
2018-05-18 18:24:39 +0200

    add Entry component

--- assets/curriculum-app/components/Entry.jsx
+++ assets/curriculum-app/components/Entry.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Entry = ({ diploma, title }) => (
-  <a className="panel-block">
-    <span className="panel-icon">
-      <i className="fa fa-graduation-cap " aria-hidden="true"></i>
-    </span>
-    { diploma || title }
-  </a>
-)
+import { DataTypes } from '../constants';
 
+const Entry = ({ diploma, title, dataType }) => {
+  let className = "fa";
+
+  switch(dataType) {
+    case DataTypes.EDUCATION:
+      className += " fa-graduation-cap";
+      break;
+    case DataTypes.EXPERIENCE:
+      className += " fa-briefcase";
+      break;
+    default:
+      throw new Error('Unknow data type: ' + dataType);
+  }
+
+  return (
+    <a className="panel-block">
+      <span className="panel-icon">
+        <i className={className} aria-hidden="true"></i>
+      </span>
+      { diploma || title }
+    </a>
+  )
+}
 
 export default Entry;
