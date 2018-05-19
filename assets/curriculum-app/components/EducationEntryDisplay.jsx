f05767487180a7d47de79adb6fa7ef7bd53d815e
Adrien Boutigny <adrien.boutigny@gmail.com>
2018-05-19 02:44:50 +0200

    improve entry display components

--- assets/curriculum-app/components/EducationEntryDisplay.jsx
+++ assets/curriculum-app/components/EducationEntryDisplay.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 
-const EducationDisplayEntry = ({ diploma }) => (
+const EducationDisplayEntry = ({
+  diploma,
+  startYear,
+  endYear,
+  description,
+  field,
+  school
+}) => (
   <nav className="panel">
-    <p className="panel-heading">{diploma}</p>
+    <p className="panel-heading">
+      <span className="icon" style={{marginRight: '.5em'}}>
+        <i className="fa fa-graduation-cap" aria-hidden="true"></i>
+      </span>
+      {diploma}
+      {' '}
+      <span className="has-text-grey" style={{fontSize: 'smaller'}}>
+        {startYear} - {endYear}
+      </span>
+    </p>
 
     <div className="panel-block">
-      diploma
+      {school}
+    </div>
+
+    <div className="panel-block">
+      {field}
+    </div>
+
+    <div className="panel-block">
+      {description}
     </div>
   </nav>
 )
--- assets/curriculum-app/components/ExperienceEntryDisplay.jsx
+++ assets/curriculum-app/components/ExperienceEntryDisplay.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import moment from 'moment';
 
-const ExperienceEntryDisplay = ({ title }) => (
+const ExperienceEntryDisplay = ({
+  title,
+  company,
+  description,
+  endDate,
+  startDate,
+  place
+}) => (
   <nav className="panel">
-    <p className="panel-heading">{title}</p>
+    <p className="panel-heading">
+    {title}
+    {' '}
+    <span className="has-text-grey" style={{fontSize: 'smaller'}}>
+    {moment(startDate).format('MMMM YYYY')} - {moment(endDate).format('MMMM YYYY')}
+    </span>
+    </p>
 
     <div className="panel-block">
-      title
+      {company}, {place}
     </div>
+    <div className="panel-block">
+      {description}
+    </div>
+
   </nav>
 )
 
