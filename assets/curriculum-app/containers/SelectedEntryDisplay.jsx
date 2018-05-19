1738d2f583463a5a99724ad088a689b393f86367
Adrien Boutigny <adrien.boutigny@gmail.com>
2018-05-19 15:47:58 +0200

    fix typo

--- assets/curriculum-app/containers/SelectedEntryDisplay.jsx
+++ assets/curriculum-app/containers/SelectedEntryDisplay.jsx
@@ -2,11 +2,11 @@ import { connect } from 'react-redux';
 
 import EntryDisplay from '../components/display/EntryDisplay';
 
-const mapStateToProps = (state, props) => {
+const mapStateToProps = (state, props) => ({
   entry: state.selectedEntry
     ? state.entries[state.selectedEntry.dataType].items[state.selectedEntry.id]
     : null
-}
+})
 
 export default connect(
   mapStateToProps
