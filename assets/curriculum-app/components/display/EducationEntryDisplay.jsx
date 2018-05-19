92663674c28e820f29bdf475aefe0a1572df7781
Adrien Boutigny <adrien.boutigny@gmail.com>
2018-05-19 03:05:37 +0200

    add type checking to the EducationEntryDisplay component

--- assets/curriculum-app/components/display/EducationEntryDisplay.jsx
+++ assets/curriculum-app/components/display/EducationEntryDisplay.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const EducationDisplayEntry = ({
   diploma,
@@ -33,5 +34,25 @@ const EducationDisplayEntry = ({
     </div>
   </nav>
 )
+
+/*
+ * Regular expression matching the ISO 8601 format
+ * source: https://www.regextester.com/97766
+ */
+const iso8601 = RegExp('^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(.[0-9]+)?(Z)?$');
+
+const checkDateFormat = (props, propName, componentName) => {
+  if(iso8601.test(props[propName])){
+    return new Error(`In ${componentName}, ${propName} doesnt conforms to the ISO 8601 format.`);
+  }
+}
+
+EducationDisplayEntry.propTypes = {
+  diploma: PropTypes.string.isRequired,
+  field: PropTypes.string.isRequired,
+  school: PropTypes.string.isRequired,
+  startYear: checkDateFormat,
+  endYear: checkDateFormat
+}
 
 export default EducationDisplayEntry;
